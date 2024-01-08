59e4fb87aa15436a80dc44de3b361fe8cbcbc57c
[Adrià Babiano] <adriafisica7777@gmail.com>
2024-01-09 00:43:27 +0100

    added alu module

---
+++ alu/src/alu.sv
@@ -0,0 +1,19 @@
+//=================================================================================
+//
+// Description    : Template for description
+// Author         : Adria Babiano
+// Created        : Jan 09 2024
+//
+//=================================================================================
+
+module alu
+	#(
+	parameter  = 
+	)(
+	input logic clk,
+	input logic rst_n,
+	input logic
+	output logic
+	);
+
+endmodule
