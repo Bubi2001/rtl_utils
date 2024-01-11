f6e0109ae0fecd6687aa9e90b8a641d8ad9cab99
[Adrià Babiano] <adriafisica7777@gmail.com>
2024-01-11 15:38:03 +0100

    added one_wire module

---
+++ one_wire/docs/one_wire.md
@@ -0,0 +1,13 @@
+# one_wire
+## Module name
+## Module parameters
+| Parameter | Type | Default Value | Description |
+| :---: | :---: | :---: | :---: |
+|  |  |  |  |
+## Module Inputs/Outputs
+| Direction | Name | Type | Description |
+| :---: | :---: | :---: | :---: |
+|  |  |  |  |
+## Packages, headers & include files
+## Test cases
+## Use cases
---
+++ one_wire/src/one_wire.sv
@@ -0,0 +1,19 @@
+//=================================================================================
+//
+// Description    : Template for description
+// Author         : Adria Babiano
+// Created        : Jan 11 2024
+//
+//=================================================================================
+
+module one_wire
+	#(
+	parameter  = 
+	)(
+	input logic clk,
+	input logic rst_n,
+	input logic
+	output logic
+	);
+
+endmodule
