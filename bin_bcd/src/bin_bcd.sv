2e57fdfae9b828ab3af85fc46ea99edec55f7c8c
[Adrià Babiano] <adriafisica7777@gmail.com>
2024-01-14 08:21:20 +0100

    added main source code

--- bin_bcd/src/bin_bcd.sv
+++ bin_bcd/src/bin_bcd.sv
@@ -6,14 +6,19 @@
 //
 //=================================================================================
 
-module bin_bcd
-	#(
-	parameter  = 
+module bin2bcd #( 
+	parameter W = 32												// input width
 	)(
-	input logic clk,
-	input logic rst_n,
-	input logic
-	output logic
-	);
-
+	input logic[W-1:0] Bin,  										// Binary
+	output logic[(W+(W-4)/3):0] Bcd 								// Bcd {...,thousands,hundreds,tens,ones}
+	); 
+	integer i,j;
+	always @(Bin) begin
+		for(i = 0; i <= W+(W-4)/3; i++) Bcd[i] = 0;					// initialize with zeros
+		Bcd[W-1:0] = Bin;                                   		// initialize with input vector
+		for(i = 0; i <= W-4; i = i+1)                       		// iterate on structure depth
+			for(j = 0; j <= i/3; j = j+1)                     		// iterate on structure width
+				if (Bcd[W-i+4*j -: 4] > 4)                      	// if > 4
+					Bcd[W-i+4*j -: 4] = Bcd[W-i+4*j -: 4] + 4'd3; 	// add 3
+	end
 endmodule
